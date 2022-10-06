f327ec4cb5cc8a400df9fe9bdc9ed16be0c76f83
quassbottle <no@bitches.com>
2022-10-06 20:53:28 +0300

    Task2 fix

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -19,5 +19,23 @@ var departments = new List<Department>()
 };
 
 var gen = new EmployeeGenerator(departments);
-var employees = gen.GenerateEmployees(10);
-var dic = employees.GroupBy(e => e.Department.Name).ToDictionary(e => e.Key, e => e.ToList());
+var employees = gen.GenerateEmployees(10).ToList();
+
+foreach (var e in employees.ToList())
+{
+    Console.WriteLine($"{e.Name} {e.Surname} - {e.Department.Name}");
+}
+
+Console.WriteLine();
+
+var dic = employees.GroupBy(e => e.Department.Name);
+
+foreach (var d in dic)
+{
+    Console.WriteLine(d.Key);
+    foreach (var e in d.ToList())
+    {
+        Console.WriteLine($"{e.Name} {e.Surname}");
+    }
+    Console.WriteLine();
+}
